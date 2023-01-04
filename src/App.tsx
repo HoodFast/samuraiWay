91de7996798261dcc87c3f9a0ef3ed9a11e42739
HoodFast <rabota-trassa@mail.ru>
2023-01-04 19:16:46 +0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,39 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      Hellow
-    </div>
-  );
+const App = () => {
+    return (
+        <div>
+            <Header/>
+            <Tehnologies />
+        </div>
+    );
+}
+
+const Header = () => {
+    return (
+        <div>
+            <a href='#s'>Home </a>
+            <a href='#s'>News Feed </a>
+            <a href='#s'>Messages </a>
+        </div>
+    );
+}
+
+const Tehnologies = () => {
+    return (
+        <div>
+            <div>
+                Hellow
+                <ul>
+                    <li> css</li>
+                    <li> html</li>
+                    <li> js</li>
+                    <li> react</li>
+                </ul>
+            </div>
+        </div>
+    );
 }
 
 export default App;
