7c0ffc0da7e0208e2750f06cf64ca0a01dd64bc7
HoodFast <rabota-trassa@mail.ru>
2023-01-10 20:01:41 +0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,29 @@ import {Header} from "./components/Header/Header";
 import {Navbar} from "./components/Navbar/Navbar";
 import {Profile} from "./components/Profile/Profile";
 import {Dialogs} from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route, Routes,} from "react-router-dom";
+
+
+
 
 
 const App = () => {
     return (
-        <div className='app-wrapper'>
-            <Header/>
-            <Navbar/>
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Header/>
+                <Navbar/>
 
-            <div className={'app-wrapper-content'}>
-                <Dialogs/>
-                {/*<Profile/>*/}
+                <div className={'app-wrapper-content'}>
+                    <Routes>
+                    <Route path="/dialog" element={<Dialogs/>}/>
+                    <Route path="/profile" element={<Profile/>}/>
+                    {/*<Dialogs/>*/}
+                    {/*<Profile/>*/}
+                    </Routes>
+                </div>
             </div>
-        </div>
+        </BrowserRouter>
     );
 }
 
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,11 +5,11 @@ import s from './Navbar.module.css'
 export const Navbar = () => {
     return (
         <nav className={s.nav}>
-            <div className={`${s.item} ${s.activ}`}><a>Profile</a></div>
-            <div className={s.item}><a>Messages</a></div>
-            <div className={s.item}><a>News</a></div>
-            <div className={s.item}><a>Music</a></div>
-            <div className={s.item}><a>Settings</a></div>
+            <div className={`${s.item} ${s.activ}`}><a href="/profile">Profile</a></div>
+            <div className={s.item}><a href="/dialog">Messages</a></div>
+            <div className={s.item}><a href="/news">News</a></div>
+            <div className={s.item}><a href="/music">Music</a></div>
+            <div className={s.item}><a href="/settings">Settings</a></div>
         </nav>
     )
 }
