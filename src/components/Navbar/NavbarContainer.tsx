1c5fe69567920167dcdcef8c198c0e7b9b5cc68f
HoodFast <rabota-trassa@mail.ru>
2023-03-17 18:21:58 +0300

    commit

--- src/components/Navbar/NavbarContainer.tsx
+++ src/components/Navbar/NavbarContainer.tsx
@@ -6,20 +6,7 @@ import {Navbar} from "./Navbar";
 import {connect} from "react-redux";
 
 
-// type navbarPropsType = {
-//     store: any
-// }
 
-//  const avbarContainer = () => {
-// // let state = props.store.getState().sidebar
-//     return (
-//         <StoreContext.Consumer>{(store)=>{
-//             let state = store.getState().sidebar
-//             return <Navbar store={state}/>
-//         }}
-//             </StoreContext.Consumer>
-//     )
-// }
 
 const mapStateToProps=(state)=>{
     return{
