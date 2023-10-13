06144a44eae0af48e9751e1813c44473ceb9d033
HoodFast <rabota-trassa@mail.ru>
2023-10-13 22:39:05 +0300

    commit

--- src/components/Profile/ProfileInfo/ProfileStatus.test.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.test.tsx
@@ -9,12 +9,51 @@ describe("ProfileStatus component", () => {
         const instance = component.getInstance();
         expect(instance.state.status).toBe('you')
     })
-    test('after creation span should be displayed with corect status',()=>{
+    test('after creation span should be displayed ', () => {
         const ReactTestRenderer = require('react-test-renderer');
         const component = ReactTestRenderer.create(<ProfileStatus active={false} status={'you'} updateStatus={() => {
         }}/>)
-        const span = component.root.findByType('span');
-
-        expect(span.length).toBe(1)
+        // eslint-disable-next-line testing-library/await-async-query
+        const span = component.root.findByType('span')
+        expect(span).not.toBeNull()
+    })
+    test('after creation input shouldn`t be displayed', () => {
+        const ReactTestRenderer = require('react-test-renderer');
+        const component = ReactTestRenderer.create(<ProfileStatus active={false} status={'you'} updateStatus={() => {
+        }}/>)
+        // eslint-disable-next-line testing-library/await-async-query
+        expect(() => {
+            // eslint-disable-next-line testing-library/await-async-query
+            component.root.findByType('input')
+        }).toThrow()
+    })
+    test('after creation span should be displayed with corect status', () => {
+        const ReactTestRenderer = require('react-test-renderer');
+        const component = ReactTestRenderer.create(<ProfileStatus active={false} status={'you'} updateStatus={() => {
+        }}/>)
+        // eslint-disable-next-line testing-library/await-async-query
+        const span = component.root.findByType('span')
+        expect(span.children[0]).toBe('you')
+    })
+    test("input should be displayed in editMode", () => {
+        const ReactTestRenderer = require('react-test-renderer');
+        const component = ReactTestRenderer.create(<ProfileStatus active={false} status={'you'} updateStatus={() => {
+        }}/>)
+        // eslint-disable-next-line testing-library/await-async-query
+        let span = component.root.findByType('span')
+        span.props.onClick();
+        // eslint-disable-next-line testing-library/await-async-query
+        let input = component.root.findByType('input')
+        expect(input.props.value).toBe('you')
+    })
+    test("callback should be called", () => {
+        const ReactTestRenderer = require('react-test-renderer');
+        const mockCallback = jest.fn()
+        const component = ReactTestRenderer.create(<ProfileStatus active={false} status={'you'}
+                                                                  updateStatus={mockCallback}/>)
+        // eslint-disable-next-line testing-library/await-async-query
+        let instance = component.getInstance()
+        instance.deactivateInput()
+        expect(mockCallback.mock.calls.length).toBe(1)
     })
 })
